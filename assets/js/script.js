fdb840ae1122ad91f9badc4fab54e594d21c0beb
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-05 02:13:33 -0700

    Add addTimeBlock function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // global constants
 const firstHour = 8; // first hour of the day is 8 (8 am)
 const lastHour = 18; // last hour of the day is 18 (6 pm)
-const hourPrefix = "#hour-" // prefix for hour element id tags, used with hour value 8 through 18
+const hourPrefix = "hour-" // prefix for hour element id tags, used with hour value 8 through 18
 
 // object variable to store daily events based on hour
 var myEvents = {};
@@ -27,61 +27,107 @@ var myEvents = {};
 
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   
   updatePageDate();
-  loadMyEvents();
-  // addTimeBlocks(); ///////////////////////////////////
+  fillTimeBlockContainer();
   formatTimeBlocks();
   revealPage();
   dateAndTime();
 
 
-   //-------------------------//
-  //  Function: dateAndTime  //
- //-------------------------//
-// sets time interval for page refresh every 1 min (60000 ms)
-  function dateAndTime() {
-    var timeInterval = setInterval(function() {
-
-      updatePageDate();
-      formatTimeBlocks(); 
-
-      console.log('tick');
-    }, 1000);
-  }
-
-    //----------------------------//
-   //  Function: updatePageDate  //
-  //----------------------------//
- // updates page with current date
+    //---------------------------------//
+   //  Function: updatePageDate       //
+  //---------------------------------//
+ // updates page with current date and time
   function updatePageDate() {
-    var currentDate = dayjs().format("MMMM D, YYYY");
-    $( "#currentDay" ).text(currentDate);
+    var currentDate = dayjs().format("dddd MMMM D, YYYY");
+    var currentTime = dayjs().format("h:mm a");
+    $( "#currentDay" ).text(currentDate + " ⸻ " + currentTime);
   }
 
 
-     //--------------------------//
-    //  Function: loadMyEvents  //
-   //--------------------------//
+     //---------------------------------//
+    //  Function: addTimeBlocks        //
+   //---------------------------------//
+  // adds timeblocks to the page
+  function fillTimeBlockContainer() {
+    var element;
+    var hourLabel;
+    for (var hour = firstHour; hour < lastHour + 1; hour++) {
+      addTimeBlock(hour);
+      // add hour label
+      // hourLabel = convert24hrTo12hr(hour);
+      // element = $( "#" + hourPrefix + hour );
+      // element.children('.hour').text(hourLabel);
+    }
+    loadMyEvents();
+    populateMyEvents();
+    
+  }
+
+
+  function addTimeBlock(h) {
+    var tbContainer = $( "#time-block-container" );
+    
+    // add time-block div
+    var timeBlockDiv = $("<div>");
+    timeBlockDiv.attr('id', hourPrefix + h);
+    timeBlockDiv.addClass('row time-block past');
+    tbContainer.append(timeBlockDiv);
+
+    // add hour div
+    var hourDiv = $("<div>");
+    hourDiv.addClass('col-2 col-md-1 hour text-center py-3');
+    timeBlockDiv.append(hourDiv);
+    hourLabel = convert24hrTo12hr(h);
+    // timeBlockDiv.text('9AM');
+
+    // add description textarea
+    var descriptionTA = $("<textarea>");
+    descriptionTA.addClass('col-8 col-md-10 description');
+    descriptionTA.attr('rows', 3);
+    timeBlockDiv.append(descriptionTA);
+
+    // add save button
+    var saveButton = $('<button>');
+    saveButton.addClass('btn saveBtn col-2 col-md-1');
+    saveButton.attr('aria-label', 'save');
+    timeBlockDiv.append(saveButton);
+
+    // add save icon
+    var saveIcon = $("<i>");
+    saveButton.addClass('fas fa-save');
+    saveButton.attr("aria-hidden", true);
+    saveButton.append(saveIcon);
+  }
+
+
+     //---------------------------------//
+    //  Function: convert24hrTo12hr    //
+   //---------------------------------//
+  // takes in 24-hour input and returns 12-hr time with AM/PM
+  function convert24hrTo12hr(inputHour) {
+    return dayjs().hour(inputHour).format("hA"); // 17 => '5PM', 9 => '9AM'
+  }
+
+
+     //---------------------------------//
+    //  Function: loadMyEvents         //
+   //---------------------------------//
   // load myEvents from localStorage, initialize if not found
   function loadMyEvents() {
     myEvents = JSON.parse(localStorage.getItem("myEventsStringify"));
 
-    // if high scores aren't saved in local storage, initialize the object variable
+    // if my events aren't saved in local storage, initialize the object variable
     if (!myEvents) {
       initializeMyEvents();
     }
   }
 
-     //--------------------------------//
-    //  Function: initializeMyEvents  //
-   //--------------------------------//
+
+     //---------------------------------//
+    //  Function: initializeMyEvents   //
+   //---------------------------------//
   // populate myEvents object variable based on start and end hours
   function initializeMyEvents() {
     myEvents = {};
@@ -92,43 +138,20 @@ $(function () {
   }
 
 
-     //---------------------------//
-    //  Function: addTimeBlocks  //
-   //---------------------------//
-  // adds timeblocks to the page
-  function addTimeBlocks() {
-    for (var hour = firstHour; hour < lastHour + 1; hour++) {
-      //add timeblock
-      // add hour label
-      $( hourPrefix + hour ).children('div').text(convert24hrTo12hr(hour));
-
-    }
-
-  }
-     //-------------------------------//
-    //  Function: convert24hrTo12hr  //
-   //-------------------------------//
-  // takes in 24-hour input and returns 12-hr time with AM/PM
-  function convert24hrTo12hr(inputHour) {
-    return dayjs().hour(inputHour).format("hA"); // 17 => '5PM', 9 => '9AM'
-  }
-
-
-     //----------------------------//
-    //  Function: updateMyEvents  //
-   //----------------------------//
+     //---------------------------------//
+    //  Function: populateMyEvents     //
+   //---------------------------------//
   // add event text to timeblocks
-  function updateMyEvents() {
+  function populateMyEvents() {
     for (var hour = firstHour; hour < lastHour + 1; hour++) {
-      $(hourPrefix + hour).children("description").text(myEvents[hour]);
+      $("#" + hourPrefix + hour).children(".description").text(myEvents[hour]);
     }
   }
 
 
-
-     //------------------------------//
-    //  Function: formatTimeBlocks  //
-   //------------------------------//
+     //---------------------------------//
+    //  Function: formatTimeBlocks     //
+   //---------------------------------//
   // formats input timeblocks by updating the timeblock class
   function formatTimeBlocks() {
     // strings used with addition of past, present, or future class
@@ -144,7 +167,7 @@ $(function () {
 
       // get element id based on the input argument
       // element = document.getElementById(hourPrefix + hour);////////////////////////////////////////
-      element = $(hourPrefix + hour);
+      element = $( "#" + hourPrefix + hour );
 
       // remove past, present, and future classes
       element.removeClass(pastStr, presentStr, futureStr);
@@ -162,53 +185,71 @@ $(function () {
   }
 
 
-     //------------------------------//
-    //  Save button event listener  //
-   //------------------------------//
-  // save button click event listener
-function revealPage() {
-  $( 'body' ).css('opacity', 1);
-}
+     //---------------------------------//
+    //  Function: revealPage           //
+   //---------------------------------//
+  // fade in the page to prevent flickering
+  function revealPage() {
+    $( 'body' ).css('opacity', 1);
+  }
+  
+  
+   //---------------------------------//
+  //  Function: dateAndTime          //
+ //---------------------------------//
+// sets time interval for page refresh every 1 min (60000 ms)
+  function dateAndTime() {
+    var timeInterval = setInterval(function() {
+
+      updatePageDate();
+      formatTimeBlocks(); 
+
+      console.log('tick');
+    }, 1000);
+  }
 
 
-     //------------------------------//
-    //  Save button event listener  //
-   //------------------------------//
+     //---------------------------------//
+    //  Save button event listener     //
+   //---------------------------------//
   // save button click event listener
-  $( "#time-block-container" ).on( "click", function( event ) {
+  $( "#time-block-container" ).on( "click", ".saveBtn", function( event ) {
     event.preventDefault();
 
     // get the hour element ID of the clicked button
-    var elementID = $(event.target).parent().attr('id');
+    var elementID = $(event.target).parents('.time-block').attr('id');
+    console.log(elementID);
+    console.log(typeof(elementID));
+
     // get the hour number
     var elementHour = Number(elementID.slice(5));
-    
+
     saveSingleEvent(elementHour);
   } );
 
-     //-----------------------------//
-    //  Function: saveSingleEvent  //
-   //-----------------------------//
+     //---------------------------------//
+    //  Function: saveSingleEvent      //
+   //---------------------------------//
   // save a single event to myEvents and local storage based on hour argument
   function saveSingleEvent(hour) {
     if (hour >= firstHour && hour <= lastHour) {
-      myEvents[hour] = $(hourPrefix + hour).children("description").text();
+      myEvents[hour] = $(hourPrefix + hour).children(".description").val();
     }
     saveMyEvents();
   }
 
-     //--------------------------//
-    //  Function: saveMyEvents  //
-   //--------------------------//
+     //---------------------------------//
+    //  Function: saveMyEvents         //
+   //---------------------------------//
   // save myEvents object variable to localStorage, notify user
   function saveMyEvents() {
     localStorage.setItem("myEventsStringify", JSON.stringify(myEvents));
     messageSaveSuccess();
   }
 
-     //--------------------------------//
-    //  Function: messageSaveSuccess  //
-   //--------------------------------//
+     //---------------------------------//
+    //  Function: messageSaveSuccess   //
+   //---------------------------------//
   // display a temporary message of success after saving
 function messageSaveSuccess() {
     var saveSuccess = $("#save-success-message");
